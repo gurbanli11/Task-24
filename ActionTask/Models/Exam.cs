119749a3ae0b1c53daa18ba61e9c78374ce79003
gurbanli11 <orkhan.80@bk.ru>
2024-04-23 16:29:48 +0400

    action

---
+++ ActionTask/Models/Exam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionTask.Models
+{
+    public class Exam
+    {
+        public string Subject { get; set; }
+        public int ExamDuration { get; set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+
+        public void Start()
+        {
+            StartDate = DateTime.Now;
+            EndDate = StartDate.AddHours(ExamDuration);
+        }
+    }
+}
---
+++ ActionTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using ActionTask.Models;
+
+namespace ActionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+        {
+            new Person { Name = "Kamil", Surname = "Aliyev", Age = 25 },
+            new Person { Name = "Kerem", Surname = "Qehramanov", Age = 30 },
+            new Person { Name = "Fuad", Surname = "Dashdemirov", Age = 20 },
+            new Person { Name = "Rauf", Surname = "Dostuyev", Age = 18 },
+            new Person { Name = "Ehmed", Surname = "Huseynov", Age = 28 },
+            new Person { Name = "Kamil", Surname = "Musayev", Age = 28 }
+        };
+            //var kamil = people.FindAll(p => p.Name == "Kamil");
+            //Console.WriteLine("Adı Kamil olanlar:");
+            //foreach (var person in kamil)
+            //{
+            //    Console.WriteLine($"{person.Name} {person.Surname}, Yaş: {person.Age}");
+            //}
+            //var surname = people.FindAll(p => p.Surname = ("ov") || p.Surname = ("ova"));
+            //Console.WriteLine("\nSoyadı ov və ova ilə bitən tələbələr:");
+            //foreach (var person in surname)
+            //{
+            //    Console.WriteLine($"{person.Name} {person.Surname}, Yaş: {person.Age}");
+            //}
+            var twenty = people.FindAll(p => p.Age > 20);
+            Console.WriteLine("\n20 yaşından böyük olanlar:");
+            foreach (var person in twenty)
+            {
+                Console.WriteLine($"{person.Name} {person.Surname}, Yaş: {person.Age}");
+            }
+        }
+
+    }
+}
